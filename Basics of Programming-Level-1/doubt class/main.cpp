3123716fe85798ab8ff39a2af49d3dc2c4c83222
Manthan-Gohil <manthangohil22577@gmail.com>
2024-07-03 14:56:18 +0530

    Basics of Programming-Level-1

---
+++ Basics of Programming-Level-1/doubt class/main.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int letsparty(int a,int b){
+    cout<<"Party started"<<endl;
+    cout<<"Party started"<<endl;
+    int sum = a+b;
+    return sum;
+}
+
+namespace Manthan
+{
+    int letsparty(int a,int b)
+    {
+    cout<<" Manthan Party started"<<endl;
+    cout<<" Manthan Party started"<<endl;
+    int sum = a+b;
+    return sum;
+    }
+}
+int main(){
+    int result = letsparty(6,5);
+    cout<<result<<endl;
+
+    int result2 = Manthan::letsparty(5,4);
+    cout<<result2<<endl;
+}
