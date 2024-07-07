f883f37bf98465166b1eb68aa5f90551fbd35466
Manthan-Gohil <manthangohil22577@gmail.com>
2024-07-07 09:32:19 +0530

    Pattern Printing

--- Basics of Programming-level-2/lets do more patterns/main.cpp
+++ Basics of Programming-level-2/lets do more patterns/main.cpp
@@ -114,6 +114,44 @@ void hollowdiamond(int n){
         cout<<endl;
     }
 }
+
+void mixpyramid(int n){
+
+    //PART 1.
+    for(int i=0;i<n;i++){
+        //stars
+        for(int j=0;j<n-i;j++){
+            cout<<"* ";
+        }
+        //space
+        for(int j=0;j<2*i+1;j++){
+            cout<<"  ";
+        }
+        //stars
+        for(int j=0;j<n-i;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+
+    //PART 2.
+    for(int i=0;i<n;i++){
+
+        //stars
+        for(int j=0;j<i+1;j++){
+            cout<<"* ";
+        }
+        //spaces
+        for(int j=0;j<2*n-2*i-1;j++){
+            cout<<"  ";
+        }
+        //stars
+        for(int j=0;j<i+1;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+}
 int main()
 {
     // fullpyramid(10);
@@ -121,5 +159,6 @@ int main()
     // diamond(5);
     // hollowpyramid(10);
     // invertedhollowpyramid(10);
-    hollowdiamond(10);
+    // hollowdiamond(10);
+    mixpyramid(10);
 }
