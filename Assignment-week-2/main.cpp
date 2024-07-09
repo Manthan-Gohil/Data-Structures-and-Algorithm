d16bab04e29cce64ab9ef672d39b4f552d3c2395
Manthan-Gohil <manthangohil22577@gmail.com>
2024-07-09 08:42:42 +0530

    Weekly Assignments

--- Assignment-week-2/main.cpp
+++ Assignment-week-2/main.cpp
@@ -18,8 +18,62 @@ void numeric_hollow_half_pyramid(int n){
     }
 }
 
+void numeric_hollow_inverted_half_pyramid(int n){
 
+    for(int i=0;i<n;i++){
+
+        for(int j=0;j<n-i;j++){
+
+            if(i==0){
+                cout<<j+1<<" ";
+            }
+            else if(j==0){
+                cout<<i+1<<" ";
+            }
+            else if(j==n-i-1){
+                cout<<n<<" ";
+            }
+            else{
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
+}
+
+void numeric_palindrome_equilateral_pyramid(int n){
+
+    int k = n;
+    for(int i=0;i<n;i++){
+        int c = 1;
+        for(int j=0;j<k;j++){
+            if(j<n-i-1){
+                cout<<" ";
+            }
+            else if(j<=n-1){
+                cout<<c;
+                c++;
+            }
+            else if(j==n){
+                c = c-2;
+                cout<<c;
+                c--;
+            }
+            else{
+                cout<<c;
+                c--;
+            }
+            
+        }
+        k++;
+        cout<<endl;
+    }
+
+
+}
 int main()
 {
-    numeric_hollow_half_pyramid(5);
+    // numeric_hollow_half_pyramid(5);
+    // numeric_hollow_inverted_half_pyramid(10);
+    numeric_palindrome_equilateral_pyramid(10);
 }
