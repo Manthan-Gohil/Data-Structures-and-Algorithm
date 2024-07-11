8db7e9b81be8b70afacd6fdf549b530b8af6e9bf
Manthan-Gohil <manthangohil22577@gmail.com>
2024-07-11 22:18:48 +0530

    Basic Arrays

--- Arrays, Time Complexity & Space Complexity/Lets learn Arrays/main.cpp
+++ Arrays, Time Complexity & Space Complexity/Lets learn Arrays/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<climits>
 using namespace std;
 
 void print(int arr[],int size){
@@ -12,6 +13,49 @@ void solve1(int arr[],int n){
         arr[i] = arr[i]*10;
     }
 }
+// Question to find the target variable in array
+bool linearsearch(int arr[],int n,int target){
+
+    for(int i=0;i<n;i++){
+        if(arr[i]==target){
+            return true;
+        }
+    }
+    return false;
+}
+//Question to find the max number in arrays
+int maxnumber(int a[],int n){
+    int MaxAns = INT_MIN;
+    for(int i=0;i<n;i++){
+        MaxAns = max(MaxAns,a[i]);
+    }
+        return MaxAns;
+}
+//Method 2 to find the max number in arrays 
+int maxnumber2(int arr[],int n){
+    int maxanswer = arr[0];
+    for(int i=1;i<n;i++){
+        if(maxanswer<arr[i]){
+            maxanswer = arr[i];
+        }
+    }
+    return maxanswer;
+}
+
+//count how many 0's and 1's in arrays
+void countnumber(int arr[],int n){
+    int count1=0,count2=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]==0){
+            count1 = count1 + 1;
+        }
+        else if(arr[i]==1){
+            count2 = count2 + 1;
+        }
+    }
+    cout<<"Total 0's: "<<count1<<" "<<"Total 1's: "<<count2<<endl;
+}
+
 int main()
 {
     //creation
@@ -42,11 +86,25 @@ int main()
     // }
 
     //array with function
-    int arr[] = {10,20,30,40,50};
-    int size = 5;
+    int arr[100];
+    int size;
+    cout<<"Enter the number of elements "<<endl;
+    cin>>size;
 
-    solve1(arr,size);
-    print(arr,size);
+    for(int i=0;i<size;i++){
+        cout<<"Enter the value at index: "<<i<<endl;
+        cin>>arr[i];
+    }
 
-    return 0;
+    // solve1(arr,size);
+    // print(arr,size);
+
+    // bool ans = linearsearch(arr,size,50);
+    // cout<<"answer: "<<ans<<endl;
+    // return 0;
+
+    // cout<<"Maximum number is: "<<maxnumber(arr,size);
+    // cout<<"Maximum number is: "<<maxnumber2(arr,size);
+    countnumber(arr,size);
+
 }
