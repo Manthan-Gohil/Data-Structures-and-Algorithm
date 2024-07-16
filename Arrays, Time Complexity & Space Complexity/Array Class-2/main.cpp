c2b6eb724feaa32688b8fc63e86a88d3cb94a944
Manthan-Gohil <manthangohil22577@gmail.com>
2024-07-16 12:55:44 +0530

    Arrays

--- Arrays, Time Complexity & Space Complexity/Array Class-2/main.cpp
+++ Arrays, Time Complexity & Space Complexity/Array Class-2/main.cpp
@@ -10,6 +10,27 @@ int findUniqueNumber(int arr[],int n){
     return ans;
 
 }
+
+void sortZeroOne(int arr[],int n){
+    int zerocount = 0;
+    int onecount = 0;
+    //counting
+    for(int i=0;i<n;i++){
+        if(arr[i]==0){
+            zerocount = zerocount + 1;
+        }
+        if(arr[i]==1){
+            onecount = onecount + 1;
+        }
+    } 
+    //insertion
+    for(int i=0;i<zerocount;i++){
+        arr[i]=0;
+    }
+    for(int i=zerocount;i<n;i++){
+        arr[i]=1;
+    }
+}
 int main(){
 
     int arr[100];
@@ -22,7 +43,13 @@ int main(){
         cin>>arr[i];
     }
 
-    cout<<"Unique number from array is: "<<findUniqueNumber(arr,size);
+    // cout<<"Unique number from array is: "<<findUniqueNumber(arr,size);
+
+    sortZeroOne(arr,size);
+    //printing
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
 
     return 0;
 }
