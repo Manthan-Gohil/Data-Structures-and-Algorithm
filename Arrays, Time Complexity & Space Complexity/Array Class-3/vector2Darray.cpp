49318084467b603ffe646813019e4bb568cbe387
Manthan-Gohil <manthangohil22577@gmail.com>
2024-07-19 11:07:52 +0530

    2D Arrays

--- Arrays, Time Complexity & Space Complexity/Array Class-3/vector2Darray.cpp
+++ Arrays, Time Complexity & Space Complexity/Array Class-3/vector2Darray.cpp
@@ -2,16 +2,37 @@
 #include<vector>
 using namespace std;
 
-int main(){
-
-    vector<vector<int>> arr (4,vector<int>(3,0));
+bool search2Darray(vector<vector<int>> arr,int target){
     int rowsize = arr.size();
     int colsize = arr[0].size();
 
     for(int i=0;i<rowsize;i++){
         for(int j=0;j<colsize;j++){
-            cout<<arr[i][j]<<" ";
+            if(arr[i][j]==target){
+               return true; 
+            }
+        }
+    }
+    return false;
+}
+int main(){
+
+    //2D array creation using vector
+    vector<vector<int>> arr (4,vector<int>(3,0));
+    
+    int rowsize = arr.size();
+    int colsize = arr[0].size();
+
+    //input
+    for(int i=0;i<rowsize;i++){
+        for(int j=0;j<colsize;j++){
+            cin>>arr[i][j];
         }
         cout<<endl;
     }
+
+    int target = 30;
+    bool ans = search2Darray(arr,target);
+    cout<<"Answer is = "<<ans<<endl;
+
 }
