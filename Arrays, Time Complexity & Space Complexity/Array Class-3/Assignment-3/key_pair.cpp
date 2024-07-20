d578de8e7ba6617a57ce756cbc74a649a494a0ca
Manthan-Gohil <manthangohil22577@gmail.com>
2024-07-20 12:51:31 +0530

    Arrays

---
+++ Arrays, Time Complexity & Space Complexity/Array Class-3/Assignment-3/key_pair.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+bool keypair(int arr[],int n){
+    int X = 16;
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            if(arr[i] + arr[j] == X){
+                return true;
+            }
+        }
+    }
+    return false;
+}
+bool twopointer(int arr[],int n){
+    //two pointer approach works only for the sorted array
+    sort(arr,arr+6); // time complexity of sort function  is O(nlogn)
+    int X = 16;
+    int low = 0;
+    int high = n-1;
+    while(low<high){
+        int sum = arr[low] + arr[high];
+        if(sum == X){
+            return true;
+        }
+        else if(sum>X){
+            high--;
+        }
+        else if(sum<X){
+            low++;
+        }
+    }
+    return false;
+}
+int main(){
+
+    int arr[6] = {1,4,45,6,10,8};
+
+    // int ans = keypair(arr,6);
+    // if(ans==1){
+    //     cout<<"Pair exists";
+    // }
+    // else{
+    //     cout<<"Pair not exist";
+    // }
+
+    
+    int ans2 = twopointer(arr,6);
+    if(ans2==1){
+        cout<<"Pair exists";
+    }
+    else{
+        cout<<"Pair not exist";
+    }
+    
+
+}
